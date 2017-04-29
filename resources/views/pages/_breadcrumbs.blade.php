bbd549a6db02ce47d866fb07629793046907f7df
Yoginth S <yoginth@zoho.com>
2017-04-29 15:30:49 +0000

    Breadcrumbs

--- resources/views/pages/_breadcrumbs.blade.php
+++ resources/views/pages/_breadcrumbs.blade.php
@@ -1,14 +1,14 @@
 <div class="breadcrumbs">
     @if (userCan('view', $page->book))
         <a href="{{ $page->book->getUrl() }}" class="text-book text-button"><i class="zmdi zmdi-book"></i>{{ $page->book->getShortName() }}</a>
-        <img src="https://doctub-cdn.netlify.com/assets/arrow.svg" style="height:13px;margin-bottom:-2px"> 
+        <img src="https://doctub-cdn.netlify.com/assets/arrow.svg" style="height:11px;margin-bottom:-1px"> 
     @endif
     @if($page->hasChapter() && userCan('view', $page->chapter))
         <a href="{{ $page->chapter->getUrl() }}" class="text-chapter text-button">
             <i class="zmdi zmdi-collection-bookmark"></i>
             {{ $page->chapter->getShortName() }}
         </a>
-        <img src="https://doctub-cdn.netlify.com/assets/arrow.svg" style="height:13px;margin-bottom:-2px"> 
+        <img src="https://doctub-cdn.netlify.com/assets/arrow.svg" style="height:11px;margin-bottom:-1px"> 
     @endif
     <a href="{{ $page->getUrl() }}" class="text-page text-button"><i class="zmdi zmdi-file"></i>{{ $page->getShortName() }}</a>
 </div>
