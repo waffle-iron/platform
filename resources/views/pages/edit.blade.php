613a5778225a9ad5059eabd94f052adb1b0b8bb2
Yoginth <yoginth@zoho.com>
2017-04-30 06:07:41 +0530

    Update edit.blade.php

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
 
 @section('head')
-    <script src="https://doctub-cdn.netlify.com/assets/tinymce.min.js"></script>
+    <script src="{{ baseUrl('/libs/tinymce/tinymce.min.js?ver=4.4.3') }}"></script>
 @stop
 
 @section('body-class', 'flexbox')
