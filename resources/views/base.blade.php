a8aafa08d2e4b0311349c21f7da38af8a1761c42
Yoginth S <yoginth@zoho.com>
2017-05-02 12:24:17 +0000

    Added Service Worker

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -106,16 +106,11 @@
       });
      </script>
      <script>
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', {scope: '/'})
-  .then(function(reg) {
-    // registration worked
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
-}
+         if('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/sw.js', {
+            scope: '/'
+           });
+         }
      </script>
 </body>
 </html>
