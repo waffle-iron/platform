eed6dd1989652630c9b7104eb38e8a1dca4f5757
Yoginth S <yoginth@zoho.com>
2017-05-02 13:50:01 +0000

    added manifest file

--- public/sw.js
+++ public/sw.js
@@ -20,11 +20,8 @@ self.addEventListener('install', e => {
   );
 });
 
-// when the browser fetches a url, either response with
-// the cached object or go ahead and fetch the actual url
 self.addEventListener('fetch', event => {
   event.respondWith(
-    // ensure we check the *right* cache to match against
     caches.open(cacheName).then(cache => {
       return cache.match(event.request).then(res => {
         return res || fetch(event.request)
