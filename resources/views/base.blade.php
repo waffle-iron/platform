1650c214ba3ae84035231af5ef52796f7a32b568
Yoginth S <yoginth@zoho.com>
2017-05-03 03:43:12 +0000

    SW.js

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -106,10 +106,5 @@
         }
       });
      </script>
-     <script>
-        if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service.js');
-        }
-     </script>
 </body>
 </html>
