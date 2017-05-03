6b1629020b10873cc1b73810d35836bd53ea1ffb
Yoginth S <yoginth@zoho.com>
2017-05-03 03:22:14 +0000

    SW.js

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -106,6 +106,10 @@
         }
       });
      </script>
-
+     <script>
+        if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js');
+        }
+     </script>
 </body>
 </html>
--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -65,6 +65,10 @@
         }
       });
      </script>
-
+     <script>
+        if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js');
+        }
+     </script>
 </body>
 </html>
