94d930acd6a742e8bf0e1ad260d6dd549f4c3972
Yoginth S <yoginth@zoho.com>
2017-05-04 09:03:51 +0000

    Commit by Workspace Bot :octocat:

--- resources/views/partials/highlight.blade.php
+++ resources/views/partials/highlight.blade.php
@@ -1,5 +1,5 @@
 
-<script src="https://doctub-cdn.netlify.com/assets/highlight.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.11.0/highlight.min.js"></script>
 <script>
     $(function() {
         var aCodes = document.getElementsByTagName('pre');
